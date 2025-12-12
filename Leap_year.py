6dde9c92f30e1a3ab8731fdda2e5dd729475e2a9
Ibsa-Magarsa <your-real-email@gmail.com>
2025-12-13 00:50:22 +0300

    edited leap year function

--- Leap_year.py
+++ Leap_year.py
@@ -13,3 +13,15 @@ def is_leap(year):
     else:
         # print("Not Leap Year. ")
         return False 
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month"
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for x in month_days:
+        if is_leap(year) and month == 2:
+         return 29
+    return month_days[month -1]
+year = int(input("Enter a year:"))
+month = int(input("Enter a month:"))
+days = days_in_month(year, month)
+print(days)
