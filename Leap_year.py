e267364c6dfb85a4671fae6cca6e9164b8ab5bc2
Ibsa-Magarsa <your-real-email@gmail.com>
2025-12-13 14:43:52 +0300

    with GUI feature

--- Leap_year.py
+++ Leap_year.py
@@ -1,3 +1,5 @@
+from tkinter import *
+from tkinter import messagebox
 def is_leap(year):
     if year % 4 == 0:
         if year %100 ==0:
@@ -14,14 +16,43 @@ def is_leap(year):
         # print("Not Leap Year. ")
         return False 
 def days_in_month(year, month):
-    if month > 12 or month < 1:
+    # year = int(input("Enter a year:"))
+    # month = int(input("Enter a month:"))
+    # days = days_in_month(year, month)
+    if month > 12 or (month) < 1:
+        messagebox.showerror(title="Oops", message="Invalid argument")
         return "Invalid month"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     for x in month_days:
         if is_leap(year) and month == 2:
-         return 29
-    return month_days[month -1]
-year = int(input("Enter a year:"))
-month = int(input("Enter a month:"))
-days = days_in_month(year, month)
-print(days)
+            messagebox.showinfo(title="Month", message= 29)
+            return 29
+            
+         
+    messagebox.showinfo(title="Month", message= f"{month_days[month -1]}")
+
+    # return month_days[month -1]
+
+
+window = Tk()
+window.title("Leap Year")
+window.config(padx= 140, pady= 100, bg="YELLOW")
+
+year_label = Label(text="Year:", bg="yellow", font=("Arial", 10, "bold"))
+year_label.grid(column=1, row=1)
+month_label = Label(text="Month:", bg="yellow", font=("Arial", 10, "bold"))
+month_label.grid(column=1, row=2)
+
+year_entry = Entry(width=30, bd=4, font=("Arial", 10, "bold"))
+# year_entry.insert(END, 0)
+year_entry.focus()
+year_entry.grid(column=2, row=1)
+month_entry = Entry(width=30, bd=4, font=("Arial", 10, "bold"))
+# month_entry.insert(END, 0)
+month_entry.grid(column=2, row=2)
+
+search_button = Button(window, text="Search",width=14, command=lambda:days_in_month(year= (int(year_entry.get())),month = int(month_entry.get())))
+search_button.grid(column=2, row=3)
+
+window.mainloop()
+# print(days)
